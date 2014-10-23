ad2c61a9bd5f17dbed30efc6dbba8d02ffc59712
Juninho <morbios@gmail.com>
2014-10-23 11:54:16 -0200

    Revert "commit serviço"
    This reverts commit 5110f5b6a43c31b6663990a35d86cc80aea2ff3b.

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,7 +67,7 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost:3306',
+		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
 		'database' => 'morarfora',
